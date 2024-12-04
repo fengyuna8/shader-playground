2ad7707168d5c7f961c10a6e2075a009c8027de3
fy <fengyuna8@outlook.com>
2024-12-04 16:39:02 +0800

    unregister event 改成正确写法

--- src/editor/message.ts
+++ src/editor/message.ts
@@ -47,7 +47,7 @@ export default class Message {
     private _unregisterEventListener<T>(messageType: MessageType, action: (messageType: MessageType, data: T) => void) {
         if (this.eventActions.has(messageType)) {
             const actions = this.eventActions.get(messageType)!
-            this.eventActions.set(messageType, actions.filter(action => action !== action))
+            this.eventActions.set(messageType, [])
         } else {
             ensure(false, `注销消息<${messageType}>不存在`)
         }
